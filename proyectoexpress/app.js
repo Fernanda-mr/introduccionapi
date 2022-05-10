f1e18ab0c1ad8dec30241fdcca2640a454ce156b
Fernanda-mr <66288449+Fernanda-mr@users.noreply.github.com>
2022-05-10 11:56:42 -0600

    creación de segundo endpoint

--- proyectoexpress/app.js
+++ proyectoexpress/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function(req, res){
+//enpoint
+app.get('/hola', function(req, res){
     res.send('Hola Mundo!');
+});
+//enpoint
+app.get('/adios', function(req, res){
+    res.send('Adios Mundo!');
 });
 
 app.listen(3000, function(){

04fae8f622ba21ce4e786481a7d3955d90791a4e
Fernanda-mr <66288449+Fernanda-mr@users.noreply.github.com>
2022-05-12 11:27:18 -0600

    actualización de usuario, uso de put

--- proyectoexpress/routes/routes.js
+++ proyectoexpress/routes/routes.js
@@ -36,5 +36,16 @@ const router = app => {
             response.status(201).send(`User added with ID: ${result.insertId}`);
         });
     });
+
+    //Actualizar unusuario ya existente uso de put
+    app.put('/users/:id', (request, response) => {
+        const id = request.params.id;
+
+        pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
+            if(error) throw error;
+
+            response.send('User updated seccessfully.');
+        });
+    });
    
 }

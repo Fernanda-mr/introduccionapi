7ecd7a6e58a266e708b766ee13912d90cd5d3d88
Fernanda-mr <66288449+Fernanda-mr@users.noreply.github.com>
2022-05-12 11:34:03 -0600

    Eliminar usuario

--- proyectoexpress/routes/routes.js
+++ proyectoexpress/routes/routes.js
@@ -47,5 +47,16 @@ const router = app => {
             response.send('User updated seccessfully.');
         });
     });
-   
+
+    //Eliminar un usuario
+    app.delete('/users/:id', (request, response) => {
+        const id = request.params.id;
+
+        pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send('User deleted.');
+        });
+    });
 }
+//Exportar el router
+module.exports = router;
